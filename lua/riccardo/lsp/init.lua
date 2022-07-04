43711b622c1aa008e0a3da48931cc146ffa3b6db
Riccardo <rshrives@turnitin.com>
2022-07-04 17:22:50 +1000

    Start from scratch

--- lua/riccardo/lsp/init.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-  return
-end
-
-require "riccardo.lsp.lsp-installer"
-require("riccardo.lsp.handlers").setup()
-require "riccardo.lsp.null-ls"
