825931c2898e06e605e9d21a33019ec0f8ae3e74
Riccardo <47872207+riccardoshrives@users.noreply.github.com>
2023-02-26 09:34:49 +1100

    NEW: Project.nvim

--- lua/ricotta/plugins.lua
+++ lua/ricotta/plugins.lua
@@ -114,6 +114,8 @@ return packer.startup(function(use)
     }
     use ("tpope/vim-surround")
     use ("tpope/vim-unimpaired")
+    use ("ahmedkhalf/project.nvim")
+
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
