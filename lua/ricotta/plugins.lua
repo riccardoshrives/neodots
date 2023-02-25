b83492b87c9a8201112366ee29ff823a53bb579a
Riccardo <47872207+riccardoshrives@users.noreply.github.com>
2023-02-26 00:07:17 +1100

    NEW: Treesitter

--- lua/ricotta/plugins.lua
+++ lua/ricotta/plugins.lua
@@ -64,6 +64,15 @@ return packer.startup(function(use)
 	  end
   })
 
+  -- Treesitter
+  use {
+	  'nvim-treesitter/nvim-treesitter',
+	  run = function()
+		  local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+		  ts_update()
+	  end,
+  }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
