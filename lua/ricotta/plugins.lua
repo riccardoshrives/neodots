19673357b0266a584e44ea17c06b780cbb485a25
Riccardo <47872207+riccardoshrives@users.noreply.github.com>
2023-02-26 23:32:04 +1100

    NEW: Autopairs

--- lua/ricotta/plugins.lua
+++ lua/ricotta/plugins.lua
@@ -129,6 +129,10 @@ return packer.startup(function(use)
     use ("tpope/vim-surround")
     use ("tpope/vim-unimpaired")
     use ("ahmedkhalf/project.nvim")
+    use {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    }
 
 
     -- Automatically set up your configuration after cloning packer.nvim
