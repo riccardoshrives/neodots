fdd9b51bc344494703b66c49baa3b40b3c1a7aa2
Riccardo <47872207+riccardoshrives@users.noreply.github.com>
2023-02-26 22:21:41 +1100

    feat: added telescope map for builtin

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -30,18 +30,26 @@ telescope.setup {
       },
     },
   },
+  pickers = {
+      colorscheme = {
+          enable_preview = true
+      }
+  }
 }
 
 telescope.load_extension "fzf"
 
 -- Keymaps for telescope
-
+local opts = { noremap = true, silent = true }
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+-- vim.keymap.set('n', '<leader>t', '<cmd>Telescope<CR>', opts)
+vim.keymap.set('n', '<leader>t', builtin.builtin, opts)
+vim.keymap.set('n', '<leader>pf', builtin.git_files, opts)
+vim.keymap.set('n', '<leader>ff', builtin.find_files, opts)
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, opts)
+vim.keymap.set('n', '<leader>fb', builtin.buffers, opts)
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, opts)
+vim.keymap.set('n', '<leader>cs', builtin.colorscheme, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
