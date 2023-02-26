91becebddd8cbfe9947b95237eae21a08ca91149
Riccardo <47872207+riccardoshrives@users.noreply.github.com>
2023-02-26 22:22:47 +1100

    change: use recommended preset for lsp zero

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,8 +1,17 @@
 local lsp = require('lsp-zero').preset({
-  name = 'minimal',
-  set_lsp_keymaps = true,
-  manage_nvim_cmp = true,
-  suggest_lsp_servers = false,
+    suggest_lsp_servers = true,
+    setup_servers_on_start = true,
+    set_lsp_keymaps = true,
+    configure_diagnostics = true,
+    cmp_capabilities = true,
+    manage_nvim_cmp = true,
+    call_servers = 'local',
+    sign_icons = {
+        error = '✘',
+        warn = '▲',
+        hint = '⚑',
+        info = ''
+    }
 })
 
 -- (Optional) Configure lua language server for neovim
