8a97f1ee804d7006abfbe4225c98668f9bf0a7c1
Riccardo <47872207+riccardoshrives@users.noreply.github.com>
2023-03-06 10:08:38 +1100

    chore: project cleanup for lazy

--- lua/ricotta/plugins/project.lua
+++ lua/ricotta/plugins/project.lua
@@ -1,3 +1,26 @@
 return {
     "ahmedkhalf/project.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim"},
+    config = function()
+        require("project_nvim").setup {
+            -- detection_methods = { "lsp", "pattern" }, -- NOTE: lsp detection will get annoying with multiple langs in one project
+            detection_methods = { "pattern" },
+
+            -- patterns used to detect root dir, when **"pattern"** is in detection_methods
+            patterns = { ".git" },
+
+            -- Don't calculate root dir on specific directories
+            exclude_dirs = {
+                "/Users/rshrives",
+            },
+        }
+
+        -- Use telescope to browse projects
+        require("telescope").load_extension "projects"
+        vim.keymap.set("n", "<leader>fp", ":Telescope projects<CR>", {
+            desc = "[F]ind [P]rojects",
+            silent = true,
+            noremap = true
+        })
+    end
 }
