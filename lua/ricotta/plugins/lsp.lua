969b5ef315a2dd61148545913d1eaaefb74b67a7
Riccardo <47872207+riccardoshrives@users.noreply.github.com>
2023-03-07 06:43:42 +1100

    chore: lsp cleanup for lazy

--- lua/ricotta/plugins/lsp.lua
+++ lua/ricotta/plugins/lsp.lua
@@ -18,5 +18,37 @@ return {
         -- Snippets
         { 'L3MON4D3/LuaSnip' }, -- Required
         { 'rafamadriz/friendly-snippets' }, -- Optional
-    }
+    },
+    config = function()
+        local lsp = require('lsp-zero').preset({
+            name = "recommended",
+            suggest_lsp_servers = true,
+            setup_servers_on_start = true,
+            set_lsp_keymaps = true,
+            configure_diagnostics = true,
+            cmp_capabilities = true,
+            manage_nvim_cmp = true,
+            manage_luasnip = true,
+            call_servers = 'local',
+            sign_icons = {
+                error = '✘',
+                warn = '▲',
+                hint = '⚑',
+                info = ''
+            }
+        })
+
+        lsp.on_attach(function(client, bufnr)
+            local opts = { buffer = bufnr }
+            local bind = vim.keymap.set
+
+            bind('n', '<leader>lf', '<cmd>LspZeroFormat<cr>', opts)
+            -- more keybindings...
+        end)
+
+        -- (Optional) Configure lua language server for neovim
+        lsp.nvim_workspace()
+
+        lsp.setup()
+    end
 }
