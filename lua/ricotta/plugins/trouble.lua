f660361e2b0cd39ea3f3c1652129940197391393
Riccardo <47872207+riccardoshrives@users.noreply.github.com>
2023-03-26 20:06:52 +1100

    NEW: folke/trouble.nvim

---
+++ lua/ricotta/plugins/trouble.lua
@@ -0,0 +1,10 @@
+return {
+    "folke/trouble.nvim",
+    config = function()
+        require("trouble").setup {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
+    end
+}
