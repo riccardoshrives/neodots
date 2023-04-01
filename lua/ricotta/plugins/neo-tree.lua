f63df8fccd6be40a952786c6b788e37c6bbc9390
Riccardo <47872207+riccardoshrives@users.noreply.github.com>
2023-04-02 07:55:40 +1000

    feat: Neotree hijacks netrw behaviour

--- lua/ricotta/plugins/neo-tree.lua
+++ lua/ricotta/plugins/neo-tree.lua
@@ -10,7 +10,13 @@ return {
         -- Unless you are still migrating, remove the deprecated commands from v1.x
         vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
-        -- 
+        require("neo-tree").setup({
+            filesystem = {
+                hijack_netrw_behavior = "open_current",
+            }
+        })
+
+        --
         vim.keymap.set("n", "\\", ":NeoTreeFocusToggle <CR>", {
             desc = "Toggle NeoTree",
             noremap = true,
